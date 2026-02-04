7e8affde7ba5cb9c32e5376727e7894518c12e01
Felipe Correa Bonanni <felipebonanni02@gmail.com>
2026-02-04 17:58:11 -0300

    Start learnin JavaScript

---
+++ javascript.js
@@ -0,0 +1,18 @@
+//variável
+let firstName="John"
+let lastName="Doe"
+
+//print
+console.log(firstName)
+console.log(lastName)
+
+let age=11
+console.log(age)
+
+age=5 //não precisa de let para reajustar, apenas para declarar
+console.log(age)
+
+const pi=3.14 //const faz a variável ser fixa, não vai mudar
+console.log(pi)
+
+console.log((3 + 2) - 76 * (1 + 1))
