9e07ffb5be1676e4f5fe70ee1ddee04cfe21dea4
Felipe Correa Bonanni <felipebonanni02@gmail.com>
2026-02-05 15:17:42 -0300

    Learn conditionals and function

--- javascript.js
+++ javascript.js
@@ -16,3 +16,18 @@ const pi=3.14 //const faz a variável ser fixa, não vai mudar
 console.log(pi)
 
 console.log((3 + 2) - 76 * (1 + 1))
+
+let variavel=10
+
+if (variavel>=5) { //condições
+    console.log("Maior ou igual a 5");
+} else {
+    console.log("Menor que 5");
+}
+
+function favoriteAnimal(animal) { //Função
+    return animal + " is my favorite animal!"
+}
+const message = favoriteAnimal("Goat")
+console.log(message)
+// console.log(favoriteAnimal("Goat")) --> também funciona, mas não salva como uma variável separada
